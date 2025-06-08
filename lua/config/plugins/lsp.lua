cd256d925abcd74f7ca482513eec69388d125aeb
Wybe242 <elikeene81@gmail.com>
2025-06-08 16:05:21 -0400

    added html lsp and snippets using chatgpt

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,4 +1,5 @@
-local path =vim.fn.expand("~/.local/share/nvim/lsp_servers")
+local path = vim.fn.expand("~/.local/share/nvim/lsp_servers")
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -13,9 +14,53 @@ return {
       },
     },
     config = function()
-      require("lspconfig").lua_ls.setup {
-	cmd = { path.."/lua-language-server/bin/lua-language-server"}
-      } 
+      local lspconfig = require("lspconfig")
+
+      -- Setup lua_ls
+      lspconfig.lua_ls.setup {
+        cmd = { path .. "/lua-language-server/bin/lua-language-server" },
+      }
+
+      -- Setup capabilities for HTML LSP
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      lspconfig.html.setup {
+        capabilities = capabilities,
+      }
+    end,
+  },
+{
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",     -- LSP source
+      "L3MON4D3/LuaSnip",         -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- LuaSnip source
+      "rafamadriz/friendly-snippets", -- Snippet collection
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      require("luasnip.loaders.from_vscode").lazy_load() -- Load friendly snippets
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }),
+      })
     end,
   },
 }
+
