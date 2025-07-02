21736aff47e02c79ecfce9c1379db86d20fc135a
Wybe242 <elikeene81@gmail.com>
2025-07-02 11:03:12 -0400

    Added file_browser to telescopea
    added the file browser extension to telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -4,16 +4,46 @@ return {
     tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      "nvim-telescope/telescope-file-browser.nvim",
     },
     config = function()
-      require('telescope').setup {}
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        },
+        extensions = {
+          fzf = {},
+          file_browser = {
+            theme = "ivy",
+            hijack_netrw = true,
+            mappings = {
+              ["n"] = {
+                vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+              },
+            }
+          }
+        },
+      }
+
+      require('telescope').load_extension('fzf')
+      require("telescope").load_extension("file_browser")
+
       vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
       vim.keymap.set("n", "<space>en", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath('config')
         }
       end)
     end
-  }
+  },
+  -- {
+  --   "nvim-telescope/telescope-file-browser.nvim",
+  --   config = function()
+  --     vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
+  --   end
+  -- }
 }
